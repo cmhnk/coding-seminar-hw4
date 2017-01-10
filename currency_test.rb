bfefba119d5d0a7861725ede35b422b1c7041994
cmhnk <corinne@automatedinsights.com>
2017-01-10 16:18:00 -0500

    can initialize currency

--- currency_test.rb
+++ currency_test.rb
@@ -1,6 +1,9 @@
 require 'minitest/pride'
 require 'minitest/autorun'
+require_relative 'currency.rb'
 
 class CurrencyTest < Minitest::Test
-
+  def test_currency_creation
+    Currency.new(amount: 34, code: 'USD')
+  end
 end
