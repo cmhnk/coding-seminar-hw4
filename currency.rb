393e37593dc7a809ede719b00ded5cd338bd6b25
cmhnk <corinne@automatedinsights.com>
2017-01-17 17:41:18 -0500

    currency object equals another currency object with the amount amount and currrency code

--- currency.rb
+++ currency.rb
@@ -1,6 +1,13 @@
 class Currency
+
+  attr_reader :amount, :code
+
   def initialize(amount:, code:)
     @amount = amount
     @code = code
+  end
+
+  def ==(other)
+    @amount == other.amount && @code == other.code
   end
 end
--- currency_test.rb
+++ currency_test.rb
@@ -6,4 +6,15 @@ class CurrencyTest < Minitest::Test
   def test_currency_creation
     Currency.new(amount: 34, code: 'USD')
   end
+
+  def test_equal_currencies
+    a = Currency.new(amount: 34, code: 'USD')
+    b = Currency.new(amount: 34, code: 'USD')
+    # the thing that comes after assert needs to be true
+    assert a == b
+
+    c = Currency.new(amount: 34, code: 'EUR')
+    # refute means that the thing that comes after must be falsy
+    refute a == c
+  end
 end
